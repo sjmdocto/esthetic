63048cbb46ca4cc97c2f2b7bd5d08557aa7cf248
Steve Docto <stevend626@gmail.com>
2020-09-29 20:35:13 -0400

    Last commit before rebuilding from the ground up

--- ios/Pods/Target Support Files/Pods-Esthetic-EstheticTests/Pods-Esthetic-EstheticTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Esthetic_EstheticTests : NSObject
-@end
-@implementation PodsDummy_Pods_Esthetic_EstheticTests
-@end
--- ios/Pods/Target Support Files/Pods-Esthetic-tvOS/Pods-Esthetic-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Esthetic_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_Esthetic_tvOS
-@end
